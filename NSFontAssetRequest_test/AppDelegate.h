a2f67eb8a1c3ea80434704eaadb4aed0c90f6d91
Gregory John Casamento <greg.casamento@gmail.com>
2025-09-06 05:02:26 -0400

    Initial Commit

---
+++ NSFontAssetRequest_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSFontAssetRequest_test
+//
+//  Created by Gregory Casamento on 9/6/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
